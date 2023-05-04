6a2732fb48c31659997c728decacb13360c3c4c0
qushu456 <132555543+qushu456@users.noreply.github.com>
2023-05-04 18:00:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '媛媛老婆',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oqMsE6UQLP4YomT2FpdGySCvgw3g',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'gGmeQkz4YXIf1ORBnSdjKUFU-LE9eBbbQ6vU0jqXODo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-16',
       festivals: [
