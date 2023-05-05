b842c5f5225748740d49da58eba4c75f6547dc83
qushu456 <132555543+qushu456@users.noreply.github.com>
2023-05-05 16:19:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqMsE6UQLP4YomT2FpdGySCvgw3g',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'gGmeQkz4YXIf1ORBnSdjKUFU-LE9eBbbQ6vU0jqXODo',
+      useTemplateId: 'TQN2CcM575utO5X3fLxLkg6uULRemTGarbXEfc4F8pM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-16',
       festivals: [
